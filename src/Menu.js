2b191db2e4acbc826d71f0c6060399157885c023
marganaraz <marganaraz@gmail.con>
2020-03-15 11:36:20 -0300

    AppTopBar

--- src/Menu.js
+++ src/Menu.js
@@ -4,8 +4,59 @@ import { Config } from './Config';
 import {Controlled as CodeMirror} from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/clike/clike';
+import AppTopBar from './AppTopBar';
+
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/styles';
 
 const clientId = Config.GoogleAPIKey;
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && <Box p={3}>{children}</Box>}
+      </Typography>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    };
+  }
+
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.paper,
+      display: 'flex',
+      height: 224,
+    },
+    tabs: {
+      borderRight: `1px solid ${theme.palette.divider}`,
+    },
+  }));
 
 class Menu extends Component {
 
@@ -14,6 +65,7 @@ class Menu extends Component {
         this.state = {
             usuario: '',
             code: '',
+            value: 0,
           };
     }
 
@@ -28,36 +80,65 @@ class Menu extends Component {
         this.props.history.push('/');
     }
 
+    handleChange = (event, newValue) => {
+        this.setState({value: newValue});
+    }
+
     render(){
-        const { usuario, code } = this.state;
+        const { usuario, code, value } = this.state;
+        const { classes } = this.props;
         return(
             <div>
                 <div>
+                    <AppTopBar />
                     <h1>{usuario}</h1>
-                </div>
-                <div>
                     <GoogleLogout buttonText="Logout" clientId={clientId} onLogoutSuccess={this.logout} />
                 </div>
                 <div>
-                <CodeMirror
-                    value={code}
-                    options={{
-                        mode: 'text/x-csharp',
-                        theme: 'default',
-                        lineNumbers: true
-                      }}
-                    onBeforeChange={(editor, data, value) => {
-                        this.setState({code: value});
-                    }}
-                     onChange={(editor, data, value) => {
-                        this.setState({code: value});
-                     }}
-/>
+                    <div className={classes.root}>
+                        <Tabs
+                            orientation="vertical"
+                            variant="scrollable"
+                            value={value}
+                            onChange={this.handleChange}
+                            aria-label="Vertical tabs example"
+                            className={classes.tabs}
+                        >
+                            <Tab label="Verify" {...a11yProps(0)} />
+                            <Tab label="Skynet" {...a11yProps(1)} />
+                            <TabPanel value={value} index={0}>
+                                Item One
+                            </TabPanel>
+                            <TabPanel value={value} index={1}>
+                                Item Two
+                            </TabPanel>
+                        </Tabs>
+                    </div>
+                    <div>
+                        
+                        <CodeMirror
+                            value={code}
+                            options={{
+                                mode: 'text/x-csharp',
+                                theme: 'default',
+                                lineNumbers: true
+                            }}
+                            onBeforeChange={(editor, data, value) => {
+                                this.setState({code: value});
+                            }}
+                            onChange={(editor, data, value) => {
+                                this.setState({code: value});
+                            }}
+                        />
+                    </div>
                 </div>
-                
             </div>
         )
     }
 }
 
-export default Menu;
+Menu.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+export default withStyles(useStyles)(Menu);
