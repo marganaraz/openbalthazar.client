f87dd6837b674c1004c16c23a3b7e0bebee74ce8
Mauro <marganaraz@juslapampa.gob.ar>
2021-04-06 08:27:28 -0300

    lib upgrade

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,5 +1,5 @@
 export const Config = { 
-    OpenBalthazarAPI: "https://localhost:44303/api",
-    //OpenBalthazarAPI: "https://openbalthazarapi.saas-pymes.com.ar/api",
+    //OpenBalthazarAPI: "https://localhost:44303/api",
+    OpenBalthazarAPI: "https://openbalthazarapi.saas-pymes.com.ar/api",
     GoogleAPIKey: "974251555483-ospq7eqj3pkrd7vmsndufuc1gmc0hnfo.apps.googleusercontent.com",
 };
